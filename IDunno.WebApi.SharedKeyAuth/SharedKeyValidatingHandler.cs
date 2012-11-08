118a71557407301d77db839d106647e5a0fc037b
Dominick Baier <dbaier@leastprivilege.com>
2012-11-08 12:50:24 +0100

    Refactored validation

--- IDunno.WebApi.SharedKeyAuth/SharedKeyValidatingHandler.cs
+++ IDunno.WebApi.SharedKeyAuth/SharedKeyValidatingHandler.cs
@@ -32,12 +32,6 @@ namespace Idunno.WebApi.SharedKeyAuthentication
     public class SharedKeyValidatingHandler : DelegatingHandler
     {
         /// <summary>
-        /// The anonymous principal which is attached if authentication headers are not present.
-        /// </summary>
-        private static readonly ClaimsPrincipal AnonymousPrincipal = 
-            new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new Claim(ClaimTypes.Name, string.Empty) }));
-
-        /// <summary>
         /// Initializes a new instance of the <see cref="SharedKeyValidatingHandler" /> class.
         /// </summary>
         public SharedKeyValidatingHandler()
@@ -83,76 +77,25 @@ namespace Idunno.WebApi.SharedKeyAuthentication
             {
                 throw new ArgumentNullException("request");
             }
+
+            try
+            {
+                var principal = SignatureValidator.Validate(request, SharedSecretResolver, MaximumMessageAge);
+                SetPrincipal(principal);
+            }
+            catch (UnauthorizedException)
+            {
+                return Unauthorized();
+            }
+            catch (ForbiddenException fbex)
+            {
+                return Forbidden(fbex.Message);
+            }
+            catch (PreconditionFailedException pcex)
+            {
+                return PreconditionFailed(pcex.Message);
+            }
             
-            AuthenticationHeaderValue authenticationHeader = null;
-            
-            if (request.Headers != null)
-            {
-                authenticationHeader = request.Headers.Authorization;
-            }
-
-            string accountName;
-            byte[] sentHmac;
-
-            if (request.Headers != null && DateTime.Now - request.Headers.Date > this.MaximumMessageAge)
-            {
-                return Forbidden("Request Expired");                
-            }
-
-            if (authenticationHeader == null || 
-                string.IsNullOrWhiteSpace(authenticationHeader.Parameter) || 
-                authenticationHeader.Scheme != SharedKeyAuthentication.Scheme || 
-                !SharedKeyAuthentication.TryParse(authenticationHeader.Parameter, out accountName, out sentHmac))
-            {
-                this.AttachPrincipal(AnonymousPrincipal);
-            }
-            else
-            {
-                byte[] sharedKey = this.SharedSecretResolver(accountName);
-
-                if (sharedKey == null || sharedKey.Length == 0)
-                {
-                    // Account not found
-                    return Unauthorized();
-                }
-
-                // Now check the checksums
-                // First, if a body is present, ensure it hasn't changed.
-                Task<byte[]> readContent = request.Content.ReadAsByteArrayAsync();
-                byte[] requestContent = readContent.Result;
-                if (requestContent.Length > 0)
-                {
-                    var sentHash = request.Content.Headers.ContentMD5;
-
-                    if (sentHash == null)
-                    {
-                        return Forbidden("Content-MD5 header must be specified when a request body is included.");                                        
-                    }
-
-                    using (var md5 = new MD5CryptoServiceProvider())
-                    {
-                        byte[] computedHash = md5.ComputeHash(requestContent);
-                        if (!CompareHash(sentHash, computedHash))
-                        {
-                            return PreconditionFailed("Content-MD5 does not match the request body.");
-                        }
-                    }
-                }
-
-                // Now check the actual auth signature.
-                byte[] calculatedHmac = SharedKeySignature.Calculate(request, accountName, sharedKey);
-
-                if (!CompareHash(sentHmac, calculatedHmac))
-                {
-                    return Forbidden("Token validation failed.");
-                }
-
-                var claims = new List<Claim> { new Claim(ClaimTypes.Name, accountName) };
-                var claimsIdentity = new ClaimsIdentity(claims, SharedKeyAuthentication.Scheme);
-                var claimsPrinciple = new ClaimsPrincipal(claimsIdentity);
-                this.AttachPrincipal(claimsPrinciple);
-            }
-
             return base.SendAsync(request, cancellationToken);
         }
 
@@ -160,7 +103,7 @@ namespace Idunno.WebApi.SharedKeyAuthentication
         /// Attaches the specified principal to the current thread and HTTP Context if one exists.
         /// </summary>
         /// <param name="principal">The principal to attach..</param>
-        protected virtual void AttachPrincipal(ClaimsPrincipal principal)
+        protected virtual void SetPrincipal(ClaimsPrincipal principal)
         {
             Thread.CurrentPrincipal = principal;
             if (HttpContext.Current != null)
@@ -196,29 +139,6 @@ namespace Idunno.WebApi.SharedKeyAuthentication
         private static Task<HttpResponseMessage> PreconditionFailed(string reason)
         {
             return Task<HttpResponseMessage>.Factory.StartNew(() => new HttpResponseMessage(HttpStatusCode.PreconditionFailed) { ReasonPhrase = reason });
-        }
-
-        /// <summary>
-        /// Compares two hashes in a time consistent manner.
-        /// </summary>
-        /// <param name="left">The first has to compare.</param>
-        /// <param name="right">The second has to compare.</param>
-        /// <returns>True if the hashes are identical, otherwise false.</returns>
-        [MethodImpl(MethodImplOptions.NoOptimization)]
-        private static bool CompareHash(byte[] left, byte[] right)
-        {
-            if (left.Length != right.Length)
-            {
-                return false;
-            }
-
-            var result = true;
-            for (var i = 0; i < left.Length; i++)
-            {
-                result = result & (left[i] == right[i]);
-            }
-
-            return result;
         }
     }
 }
